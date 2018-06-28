8c112fe8b17cce55077a94f3e804a9cf9ce5d5b7
Mauchamp Damien <damien.mauchamp@apivia-courtage.fr>
2018-06-28 17:32:49 +0200

    fonts

--- index.php
+++ index.php
@@ -10,33 +10,42 @@ $root = "";
     <? include "inc/meta.php"; ?>
 </head>
 <body class="<?= $theme ?>">
-<? include "inc/nav.php"; ?>
+<div class="main">
+    <? include "inc/nav.php"; ?>
 
-<section class="section-add-artists">
-    <select class="add-artists" id="artists[]" name="artists[]" multiple="multiple" style="width: 100%;"></select>
-    <label for="artists[]" class="add-artists-label --invisible"></label>
-    <div class="add-artists-label-after">Ajouter</div>
-</section>
+    <section class="main-header l-content-width section" style="padding-bottom:0;border-top:none">
+        <h1 class="section__headline--hero"><?= $news ? "Refresh" : "Releases" ?></h1>
 
-<section class="main-header l-content-width">
-    <h1 class="section__headline--hero"><?= $news ? "Refresh" : "Releases" ?></h1>
-</section>
-
-<? if ($news) : ?>
-
-    <section class="l-content-width section">
         <h2 class="section__headline">
-            New albums
+            Ajouts d'artistes
         </h2>
-
-        <div class="l-row">
-            <? getAllNewReleases(); ?>
+        <div class="section-add-artists">
+            <select class="add-artists" id="artists[]" name="artists[]" multiple="multiple"
+                    style="width: 100%;"></select>
+            <label for="artists[]" class="add-artists-label --invisible"></label>
+            <div class="add-artists-label-after">Ajouter</div>
         </div>
-
-        <div id="ember1200" style="display: none;"
-             class="we-loading-spinner we-loading-spinner--see-all ember-view"></div>
     </section>
 
-<? else : getAllAlbums($page); endif ?>
+    <section class="l-content-width section">
+    </section>
+
+    <? if ($news) : ?>
+
+        <section class="l-content-width section">
+            <h2 class="section__headline">
+                New albums
+            </h2>
+
+            <div class="l-row">²
+                <? getAllNewReleases(); ?>
+            </div>
+
+            <div id="ember1200" style="display: none;"
+                 class="we-loading-spinner we-loading-spinner--see-all ember-view"></div>
+        </section>
+
+    <? else : getAllAlbums($page); endif ?>
+</div>
 </body>
 </html>
