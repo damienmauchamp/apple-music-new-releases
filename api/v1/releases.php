a283f87ce85e8b4ec7497316c9cd1c886848cbef
Damien Mauchamp <damienmauchamp@gmail.com>
2019-11-04 18:04:30 +0100

    api init

--- api/v1/releases.php
+++ api/v1/releases.php
@@ -1,19 +1,31 @@
 <?php
 
-
-class Test
-{
-	public function getUserAlbums() {
-		return "hey";
-	}
-}
-
 $routes = [
 	'/releases' => "Test:getUserAlbums"
 ];
 
-exit();
+class Test
+{
+	public function getUserAlbums($params = []) {
+		return "Params: " . print_r($params, 1);
+	}
+}
+
+$class_method = $routes[$api_request->infos->route];
+$cm_array = explode(':', $class_method);
+
+// instanciation
+eval("\$abc = new $cm_array[0];");
+
+// exec
+eval("echo \$abc->$cm_array[1](\$api_request->params);");
+
 echo json_encode($api_request);
+// exec
+
+
+
+exit();
 
 $test = "Test::getUserAlbums();";
 $x = eval($test);
