c95a53210a3a4bfd03eae79b98560e5fe4c2b94b
Damien Mauchamp <damienmauchamp@gmail.com>
2020-01-10 09:23:26 +0100

    API: min release date + exception

--- api/v1/releases.php
+++ api/v1/releases.php
@@ -1,20 +1,34 @@
 <?php
 // exec
+$now = new DateTime();
 
 $start_date = array_key_exists('start_date', $_GET) ? $_GET['start_date'] : date('Y-m-d 00:00:00');
+$min_release_date = array_key_exists('min_release_date', $_GET) ? $_GET['min_release_date'] : $now->sub(new DateInterval('P1M'))->format('Y-m-d 00:00:00');
 $only_explicit = array_key_exists('only_explicit', $_GET) ? boolval($_GET['only_explicit']) : true;
+
+// start_date
 try {
 	$start_date = new \DateTime($start_date);
 } catch(Exception $e) {
     http_response_code(500);
-	exit($e);
+    exit("Error : '{$start_date}' is not a valid date.");
+	//exit($e);
+}
+
+// start_date
+try {
+	$min_release_date = new \DateTime($min_release_date);
+} catch(Exception $e) {
+    http_response_code(500);
+    exit("Error : '{$min_release_date}' is not a valid date.");
+	//exit($e);
 }
 
 $db = new AppleMusic\DB();
 $sql = "
 	SELECT *
 	FROM albums a
-	WHERE a.added > '".$start_date->format('Y-m-d H:i:s')."'
+	WHERE a.added > '".$start_date->format('Y-m-d H:i:s')."' AND a.date >= {$min_release_date}
 	ORDER BY a.added ASC, a.explicit DESC";
 $res = $db->selectPerso($sql);
 
