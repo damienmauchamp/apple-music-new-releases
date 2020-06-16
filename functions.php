fdf8fad76a21ad46c2cd4f8cc88c03654c7749d3
damienmauchamp <damienmauchamp@gmail.com>
2020-06-16 09:04:39 +0200

    new songs first

--- functions.php
+++ functions.php
@@ -287,6 +287,9 @@ function getAllSongs($filtrer_albums = false, $only_explicit = true, $type = nul
 //		foreach ($artists as $artist) {
 //			Artist::withNewRelease($artist)->toString();
 //		}
+		if ($type === 1) {
+			$releases_array = array_reverse($releases_array);
+		}
 	}
 
 	return $releases_array;
